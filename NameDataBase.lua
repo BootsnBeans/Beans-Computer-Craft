7451ae5a94e0f66fd0e496ab4ceda2354ac5a86a
BootsnBeans <119915203+BootsnBeans@users.noreply.github.com>
2025-08-03 13:29:43 -0600

    Create NameDataBase.lua

---
+++ NameDataBase.lua
@@ -0,0 +1,10 @@
+Rednet.Open("Right")
+--Turtle Network
+TN = "Con"
+--Names
+Names = {Apollo, Artemis, Aries, Aprodite, Hermes, Hephaestus, Hades, Zeus, Posidion, Mars, Pluto, Jupiter, Terra, Luna, Sol, B, E, A, Ruby, Olive, Nova, Star}
+while True do
+local id, msg, channel = Rednet.Receive(TN)
+if string.match(msg, "Name-TID.*") then
+Names[msg:sub("10")
+Rednet.Broadcast(
