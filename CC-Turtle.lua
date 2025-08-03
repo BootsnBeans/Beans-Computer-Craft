ae60221abf9277ef7850281b1a84a790987f1780
BootsnBeans <119915203+BootsnBeans@users.noreply.github.com>
2025-08-03 13:08:23 -0600

    Update CC-Turtle.lua

--- CC-Turtle.lua
+++ CC-Turtle.lua
@@ -1,2 +1,9 @@
 Rednet.Open("Back")
-Rednet.Broadcast("Name-TID:01","Con
+//Turtle ID
+TID = 01
+//Turtle Network
+TN = "Con"
+
+Rednet.Broadcast("Name-TID:"...TID,"Con")
+While True Do
+local id, msg, channel = Rednet.Receive("Con
