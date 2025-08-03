1aa103702b8fd4242ee0d42e7fcab97f0d567833
BootsnBeans <119915203+BootsnBeans@users.noreply.github.com>
2025-08-03 13:32:08 -0600

    Update NameDataBase.lua

--- NameDataBase.lua
+++ NameDataBase.lua
@@ -5,6 +5,6 @@ TN = "Con"
 Names = {Apollo, Artemis, Aries, Aprodite, Hermes, Hephaestus, Hades, Zeus, Posidion, Mars, Pluto, Jupiter, Terra, Luna, Sol, B, E, A, Ruby, Olive, Nova, Star}
 while True do
 local id, msg, channel = Rednet.Receive(TN)
-if string.match(msg, "Name-TID.*") then
-Names[msg:sub("10")
-Rednet.Broadcast(
+if string.match(msg, "Name-TID:.*") then
+Names[msg:sub("10")]
+Rednet.Broadcast(msg:sub("10")..." Name
